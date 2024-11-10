4237acd980a5b259c82e65b4ffc6ea466497d55c
Rahul Bhadoriya <rahul@Rahuls-MacBook-Pro.local>
2024-11-11 00:25:40 +0530

    add movies api nowPlaying

--- src/pages/MoviesList.js
+++ src/pages/MoviesList.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card } from '../components'
 
 export const MoviesList = () => {
+    const [movies, setMovies] = useState([])
+    const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTkzNDY5OWZkZWY5ZjdlNDMyMjMyYTJkNGEzYWJmYiIsIm5iZiI6MTczMTE0OTI3NC4yODUwMzEsInN1YiI6IjY3MmYzNGQ5MTc3MGIwMzUxZGUzNzhkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iTSRQvqh9tqn1sM9N5tgdMqJNeYNOezuVYKmzZyPMqk'
+      }
+    };
+    
+    useEffect(()=>{
+        async function fetchMovies(){
+            let response = await fetch(url, options)
+            let data = await response.json()
+            console.log(data.results)
+            setMovies(data.results)
+        }
+        fetchMovies()
+    }, [])
     return (
         <main>
             <section className='max-w-7xl mx-auto py-7'>
                 <div className='flex justify-start flex-wrap'>
-                    <Card />
-                    <Card />
-                    <Card />
+                    {movies.map((movie)=>(
+                      <Card key={movie.id} movie= {movie}/>
+                    ))}
                 </div>
             </section>
         </main>
